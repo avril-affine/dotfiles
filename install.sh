4f39efd938471b86598b034fe28fa58b359d3aac
kkihara <kkihara1@gmail.com>
2017-01-22 20:33:21 -0800

    backup original vim/vimrc

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+if [ -e .vimrc ]; then mv .vimrc .vimrc.bak; fi
+if [ -e .vim ]; then mv .vim .vim.bak; fi
+
 ln -s $(pwd)/tmux/tmux.conf.symlink ~/.tmux.conf
 ln -s $(pwd)/vim/vimrc.symlink ~/.vimrc
 ln -s $(pwd)/vim/vimfolder.symlink/ ~/.vim
