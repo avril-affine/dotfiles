a9f2a1d64c8259e1b5f2126c1fb09b1d6e85d86b
kenneth <kenneth@foghorn-systems.com>
2017-01-26 11:39:48 -0800

    fixed bug in install

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-if [ -e .vimrc ]; then mv .vimrc .vimrc.bak; fi
-if [ -e .vim ]; then mv .vim .vim.bak; fi
-
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+if [ -e "$HOME/.vimrc" ]; then mv "$HOME/.vimrc" "$HOME/.vimrc.bak"; fi
+if [ -e "$HOME/.vim" ]; then mv "$HOME/.vim" "$HOME/.vim.bak"; fi
 
 ln -s $(pwd)/tmux/tmux.conf.symlink ~/.tmux.conf
 ln -s $(pwd)/vim/vimrc.symlink ~/.vimrc
 ln -s $(pwd)/vim/vimfolder.symlink/ ~/.vim
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # Install plugins from Vundle
 vim +PluginInstall +qall
