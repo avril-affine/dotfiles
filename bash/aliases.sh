6547c6f1438f27989d6ce94dcd6111da2b23afb3
kenny <kkihara1@gmail.com>
2018-01-05 22:21:32 -0800

    [bash] make alias file

---
+++ bash/aliases.sh
@@ -0,0 +1,11 @@
+# ls
+alias ll="ls -lrth"
+
+alias tns="tmux new -s"
+alias ta="tmux attach"
+alias tat="tmux attach -t"
+alias tks="tmux kill-session -t"
+
+# git
+alias gco="git checkout"
+alias gsh="git stash"
