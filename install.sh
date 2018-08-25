510b332655de857c09630ef41e634ff13bd13225
kenny <kkihara1@gmail.com>
2018-08-25 10:53:03 -0700

    more install updates

--- install.sh
+++ install.sh
@@ -2,6 +2,11 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# .bashrc additions
+ln -s "$DIR/bash/bashrc" "$HOME/.bashrc.local"
+ln -s "$DIR/bash/aliases.sh" "$HOME/.aliases.sh"
+echo "source $HOME/.bashrc.local" >> "$HOME/.bashrc"
+
 if [ -e "$HOME/.tmux.conf" ]; then mv "$HOME/.tmux.conf" "$HOME/.tmux.conf.bkp"; fi
 if [ -e "$HOME/.tmux.conf.local" ]; then mv "$HOME/.tmux.conf.local" "$HOME/.tmux.conf.local.bkp"; fi
 if [ -e "$HOME/.vimrc" ]; then mv "$HOME/.vimrc" "$HOME/.vimrc.bkp"; fi
@@ -13,13 +18,15 @@ ln -s "$DIR/vim/vimrc.symlink" ~/.vimrc
 ln -s "$DIR/vim/vimfolder.symlink/" ~/.vim
 ln -s "$DIR/bash/sshrc" ~/.ssh/rc
 
+# pylint
+ln -s "$DIR/pylintrc" "$HOME/.pylintrc"
+
+# .gitconfig
+ln -s "$DIR/gitconfig" "$HOME/.gitconfig"
+
 # Package dependencies / OS specific stuff
 if uname -s | grep --quiet Linux; then
-    # YouCompleteMe
-    sudo apt-get install -y build-essential cmake
-    sudo apt-get install -y python-dev python3-dev python-pip python3-pip
-
-    # tmux
+    # tmux dependencies
     sudo apt-get install -y automake
     sudo apt-get install -y build-essential
     sudo apt-get install -y pkg-config
@@ -38,13 +45,13 @@ if uname -s | grep --quiet Linux; then
     sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
     sudo update-alternatives --config editor
 
-    # ag
-    sudo apt-get install silversearcher-ag
-
     # rg
     # Find updates here: https://github.com/BurntSushi/ripgrep/releases/latest
-    curl -LO https://github.com/BurntSushi/ripgrep/releases/download/0.8.1/ripgrep_0.8.1_amd64.deb
-    sudo dpkg -i ripgrep_0.8.1_amd64.deb
+    which -s rg
+    if [[ $? != 0 ]] ; then
+        curl -LO https://github.com/BurntSushi/ripgrep/releases/download/0.8.1/ripgrep_0.8.1_amd64.deb
+        sudo dpkg -i ripgrep_0.8.1_amd64.deb
+    fi
 else
     # Install Homebrew
     which -s brew
@@ -107,31 +114,16 @@ else
 fi
 
 # Neovim
-mkdir -p "$HOME/.config"
-ln -s ~/.vim ~/.config/nvim
-ln -s ~/.vimrc ~/.config/nvim/init.vim
-
-pip install neovim
-
-# Install plugins from Vundle
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim +PluginInstall +qall
-
-# YouCompleteMe
-cd ~/.vim/bundle/YouCompleteMe
-./install.py --clang-completer
-cd $OLDPWD
-
-# .bashrc additions
-ln -s "$DIR/bash/bashrc" "$HOME/.bashrc.local"
-ln -s "$DIR/bash/aliases.sh" "$HOME/.aliases.sh"
-echo "source $HOME/.bashrc.local" >> "$HOME/.bashrc"
-
-# pylint
-ln -s "$DIR/pylintrc" ~/.pylintrc
-
-# .gitconfig
-ln -s "$DIR/gitconfig" ~/.gitconfig
+which -s nvim
+if [[ $? != 0 ]] ; then
+    echo "Setting up nvim directories..."
+    mkdir -p "$HOME/.config"
+    ln -s ~/.vim ~/.config/nvim
+    ln -s ~/.vimrc ~/.config/nvim/init.vim
+else
+    echo "Skipping nvim directory setup..."
+fi
+pip install "neovim>=0.2"
 
 # tmux 2.6
 git clone https://github.com/tmux/tmux.git ~/tmux
