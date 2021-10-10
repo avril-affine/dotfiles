2f4a19c1e85656738fdf40e1c8cbf4bc35a0ff64
kenny <kkihara1@gmail.com>
2021-10-10 10:34:08 -0700

    lua config

--- vim/vim.symlink/lua/plugins.lua
+++ vim/vim.symlink/lua/plugins.lua
@@ -4,45 +4,108 @@ vim.cmd [[set packpath=/tmp/nvim/site]]
 local package_root = '/tmp/nvim/site/pack'
 local install_path = package_root .. '/packer/start/packer.nvim'
 
-local function lock_package(package, ...)
-  local res = { package, lock = true }
-  if select('#', ...) > 0 then
-    for k,v in pairs(...) do
-      res[k] = v
-    end
-  end
-  return res
-end
-
+-- FIXME: am i using config properly
 local function load_plugins()
-  require('packer').startup {
-    {
-      lock_package('wbthomason/packer.nvim'),
+  local packer = require('packer')
+  local use = packer.use
+  require('packer').startup ({function()
+    use 'wbthomason/packer.nvim'
 
-      -- LSP / completion
-      lock_package('neovim/nvim-lspconfig'),
-      lock_package('nvim-lua/completion-nvim'),
+    -- python
+    use { 'numirias/semshi', ft = 'python', config = 'vim.cmd [[UpdateRemotePlugins]]' }
 
-      -- file utils
-      lock_package('nvim-telescope/telescope.nvim', {requires = {'nvim-lua/plenary.nvim'}}),
-    },
+    -- LSP / completion
+    use { 'neovim/nvim-lspconfig', lock = true }
+    use { 'nvim-lua/completion-nvim', lock = true }
+    use {
+      'folke/trouble.nvim',
+      requires = { 'kyazdani42/nvim-web-devicons' },
+      config = function()
+        require('trouble').setup {
+          -- your configuration comes here
+          -- or leave it empty to use the default settings
+          -- refer to the configuration section below
+        }
+      end,
+      lock = true,
+    }
+    use {
+      'folke/todo-comments.nvim',
+      requires = { 'nvim-lua/plenary.nvim' },
+      config = function()
+        require('todo-comments').setup {
+          -- your configuration comes here
+          -- or leave it empty to use the default settings
+          -- refer to the configuration section below
+        }
+      end,
+      lock = true,
+    }
+
+    -- file utils
+    use { 'nvim-telescope/telescope.nvim', requires = { 'nvim-lua/plenary.nvim' }, lock = true }
+    use {
+      'nvim-telescope/telescope-fzf-native.nvim',
+      run = 'make',
+      config = function()
+        require('telescope').setup {
+          case_mode = 'respect_case'
+        }
+      end,
+      lock = true,
+    }
+
+    use {
+      'kyazdani42/nvim-tree.lua',
+      requires = { 'kyazdani42/nvim-web-devicons' },
+      config = function()
+        require'nvim-tree'.setup {
+        }
+        vim.api.nvim_set_keymap('n', '<leader>k', ':NvimTreeToggle<CR>', { nowait = true })
+      end,
+      lock = true,
+    }
+    use {
+      'kdheepak/lazygit.nvim',
+      config = function()
+        vim.api.nvim_set_keymap('n', '<leader>l', ':LazyGit<CR>', { nowait = true })
+      end,
+      lock = true,
+    }
+    use { 'tpope/vim-fugitive', lock = true }
+    use { 'ruanyl/vim-gh-line', lock = true }
+
+    -- utils
+    use { 'terryma/vim-smooth-scroll', lock = true }
+    use { 'machakann/vim-swap', lock = true }
+    use {
+      'hoob3rt/lualine.nvim',
+      requires = {'kyazdani42/nvim-web-devicons', opt = true},
+      lock = true,
+    }
+    use {
+      'yamatsum/nvim-nonicons',
+      requires = {'kyazdani42/nvim-web-devicons'},
+      config = function()
+        require('nvim-nonicons').get('file')
+      end,
+    }
+
+    end,
     config = {
       package_root = package_root,
       compile_path = install_path .. '/plugin/packer_compiled.lua',
-    },
-  }
+    }
+	})
 end
 
 _G.load_config = function()
   -- LSP
   local lspconfig = require('lspconfig')
+  -- TODO: Why do I define on_attach here?
   local on_attach = function(_, bufnr)
-    local function buf_set_keymap(...)
-      vim.api.nvim_buf_set_keymap(bufnr, ...)
-    end
-    local function buf_set_option(...)
-      vim.api.nvim_buf_set_option(bufnr, ...)
-    end
+    local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+	  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
 
     buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
 
@@ -51,8 +114,7 @@ _G.load_config = function()
 
     buf_set_keymap('n', '<leader>d', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
     buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-    buf_set_keymap('n', '<leader>i', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-    buf_set_keymap('n', '<leader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+    buf_set_keymap('n', '<leader>i', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts) buf_set_keymap('n', '<leader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
     buf_set_keymap('n', '<leader>r', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
     buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
     buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
@@ -75,38 +137,83 @@ _G.load_config = function()
   local sumneko_binary_path = vim.fn.exepath('lua-language-server')
   local sumneko_root_path = vim.fn.fnamemodify(sumneko_binary_path, ':h:h:h')
   local runtime_path = vim.split(package.path, ';')
-  table.insert(runtime_path, "lua/?.lua")
-  table.insert(runtime_path, "lua/?/init.lua")
+  table.insert(runtime_path, 'lua/?.lua')
+  table.insert(runtime_path, 'lua/?/init.lua')
   require'lspconfig'.sumneko_lua.setup {
     on_attach = on_attach,
-    cmd = {sumneko_binary_path, "-E", sumneko_root_path .. "/main.lua"};
+    cmd = {sumneko_binary_path, '-E', sumneko_root_path .. '/main.lua'};
     settings = {
         Lua = {
-        runtime = {
-            version = 'LuaJIT',
-            path = runtime_path,
-        },
-        diagnostics = {
-            -- Get the language server to recognize the `vim` global
-            globals = {'vim'},
-        },
-        workspace = {
-            -- Make the server aware of Neovim runtime files
-            library = vim.api.nvim_get_runtime_file("", true),
-        },
-        -- Do not send telemetry data containing a randomized but unique identifier
-        telemetry = {
-            enable = false,
-        },
+          runtime = {
+              version = 'LuaJIT',
+              path = runtime_path,
+          },
+          diagnostics = {
+              -- Get the language server to recognize the `vim` global
+              globals = {'vim'},
+          },
+          workspace = {
+              -- Make the server aware of Neovim runtime files
+              library = vim.api.nvim_get_runtime_file('', true),
+          },
+          -- Do not send telemetry data containing a randomized but unique identifier
+          telemetry = {
+              enable = false,
+          },
         },
     },
   }
 
-  -- telescope
   local opts = { noremap = true, silent = true }
+  -- smooth scroll
+  vim.api.nvim_set_keymap('n', '<C-U>', ':call smooth_scroll#up(&scroll, 0, 3)<CR>', opts)
+  vim.api.nvim_set_keymap('n',  '<C-D>', ':call smooth_scroll#down(&scroll, 0, 3)<CR>', opts)
 
-  vim.api.nvim_set_keymap('n', '<leader>f', "<cmd>lua require('telescope.builtin').live_grep()<CR>", opts)
-  vim.api.nvim_set_keymap('n', '<C-P>', "<cmd>lua require('telescope.builtin').find_files()<CR>", opts)
+  -- telescope
+  vim.api.nvim_set_keymap('n', '<leader>f', '<cmd>lua require("telescope.builtin").live_grep()<CR>', opts)
+  vim.api.nvim_set_keymap('n', '<C-P>', '<cmd>lua require("telescope.builtin").find_files()<CR>', opts)
+  require('telescope').setup {
+    fuzzy = true,
+    override_generic_sorter = true,
+    override_file_sorter = true,
+    case_mode = 'respect_case',
+  }
+  require('telescope').load_extension('fzf')
+
+  -- lualine
+  require('plenary.reload').reload_module('lualine', true)
+  require'lualine'.setup {
+  options = {
+    icons_enabled = true,
+    theme = 'gruvbox',
+    component_separators = {'', ''},
+    section_separators = {'', ''},
+    disabled_filetypes = {}
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch'},
+    lualine_c = {'filename'},
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {'branch'},
+    lualine_c = {'filename'},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  extensions = {}
+  }
+
+  -- ghline
+  vim.g.gh_line_blame_map = '<leader>g'
+
+  -- todo-comments
 
 end
 
