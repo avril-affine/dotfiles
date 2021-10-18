4cbb6bd97c4a36b91293ccaa654e1f0af5fcf6a4
kenny <kkihara1@gmail.com>
2021-10-17 21:35:24 -0700

    update

--- vim/vim.symlink/lua/plugins.lua
+++ vim/vim.symlink/lua/plugins.lua
@@ -17,27 +17,121 @@ local function load_plugins()
       lock = true,
       config = function() vim.cmd('UpdateRemotePlugins') end,
     }
+    use { 'psf/black', ft = 'python', lock = true }
+    use {
+      'stsewd/isort.nvim',
+      ft = 'python',
+      lock = true,
+      config = function()
+        vim.cmd('UpdateRemotePlugins')
+        vim.g.isort_command = 'Isort'
+      end,
+    }
 
     -- LSP / completion
     use { 'neovim/nvim-lspconfig', lock = true }
     use {
-      'nvim-lua/completion-nvim',
+      'hrsh7th/nvim-compe',
       lock = true,
+      requires = {
+        { 'hrsh7th/vim-vsnip', lock = true },
+        { 'ray-x/lsp_signature.nvim', lock = true },
+      },
       config = function()
-        vim.cmd [[
-          autocmd BufEnter * lua require('completion').on_attach()
+        require'compe'.setup {
+          enabled = true;
+          autocomplete = true;
+          debug = false;
+          min_length = 1;
+          preselect = 'enable';
+          throttle_time = 80;
+          source_timeout = 200;
+          resolve_timeout = 800;
+          incomplete_delay = 400;
+          max_abbr_width = 100;
+          max_kind_width = 100;
+          max_menu_width = 100;
+          documentation = {
+            border = { '', '' ,'', ' ', '', '', '', ' ' }, -- the border option is the same as `|help nvim_open_win|`
+            winhighlight = "NormalFloat:CompeDocumentation,FloatBorder:CompeDocumentationBorder",
+            max_width = 120,
+            min_width = 60,
+            max_height = math.floor(vim.o.lines * 0.3),
+            min_height = 1,
+          };
 
-          inoremap <expr> <Tab>   pumvisible() ? "\<C-n>" : "\<Tab>"
-          inoremap <expr> <S-Tab> pumvisible() ? "\<C-p>" : "\<S-Tab>"
-          imap <tab> <Plug>(completion_smart_tab)
+          source = {
+            path = true;
+            buffer = true;
+            calc = true;
+            nvim_lsp = true;
+            nvim_lua = true;
+            vsnip = true;
+            ultisnips = true;
+            luasnip = true;
+          };
+        }
 
-          set completeopt=menuone,noinsert,noselect
+        -- tab completion
+        local t = function(str) return vim.api.nvim_replace_termcodes(str, true, true, true) end
 
-          set shortmess+=c
-        ]]
-        vim.api.nvim_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+        local check_back_space = function()
+            local col = vim.fn.col('.') - 1
+            return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
+        end
+
+        -- Use (s-)tab to:
+        --- move to prev/next item in completion menuone
+        --- jump to prev/next snippet's placeholder
+        _G.tab_complete = function()
+          if vim.fn.pumvisible() == 1 then
+            return t "<C-n>"
+          elseif vim.fn['vsnip#available'](1) == 1 then
+            return t "<Plug>(vsnip-expand-or-jump)"
+          elseif check_back_space() then
+            return t "<Tab>"
+          else
+            return vim.fn['compe#complete']()
+          end
+        end
+        _G.s_tab_complete = function()
+          if vim.fn.pumvisible() == 1 then
+            return t "<C-p>"
+          elseif vim.fn['vsnip#jumpable'](-1) == 1 then
+            return t "<Plug>(vsnip-jump-prev)"
+          else
+            -- If <S-Tab> is not working in your terminal, change it to <C-h>
+            return t "<S-Tab>"
+          end
+        end
+
+        vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
+        vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
+        vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
+        vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
+
+        -- function signature
+        require('lsp_signature').setup()
       end
     }
+    -- use {
+    --   'nvim-lua/completion-nvim',
+    --   lock = true,
+    --   config = function()
+    --     vim.cmd [[
+    --       autocmd BufEnter * lua require('completion').on_attach()
+
+    --       inoremap <expr> <Tab>   pumvisible() ? "\<C-n>" : "\<Tab>"
+    --       inoremap <expr> <S-Tab> pumvisible() ? "\<C-p>" : "\<S-Tab>"
+    --       imap <tab> <Plug>(completion_smart_tab)
+
+    --       set completeopt=menuone,noinsert,noselect
+
+    --       set shortmess+=c
+    --     ]]
+    --     vim.api.nvim_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+    --   end
+    -- }
     use {
       'folke/trouble.nvim',
       requires = { 'kyazdani42/nvim-web-devicons', lock = true },
@@ -205,6 +299,7 @@ _G.load_config = function()
     buf_set_keymap('n', '<leader>r', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
     buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
     buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+    buf_set_keymap('n', '<leader>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
 
     -- buf_set_keymap('n', '<leader>D', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
     -- buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
@@ -212,7 +307,6 @@ _G.load_config = function()
     -- buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
     -- buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
     -- buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-    -- buf_set_keymap('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
     -- buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
 
   end
