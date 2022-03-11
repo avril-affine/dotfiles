d36a834275198f8414f28c5931954642f78365a8
kenny <kkihara1@gmail.com>
2022-03-10 21:20:32 -0800

    textobjects

--- vim/vim.symlink/lua/plugins.lua
+++ vim/vim.symlink/lua/plugins.lua
@@ -196,21 +196,15 @@ return require('packer').startup {
       }
       use {
         'nvim-treesitter/nvim-treesitter',
+        requires = { 'nvim-treesitter/nvim-treesitter-textobjects' },
+        run = ':TSUpdate',
         config = function()
           require('nvim-treesitter.configs').setup {
-            ensure_installed = { 'lua', 'javascript' },  -- can specify "all"
+            ensure_installed = { 'lua', 'javascript', 'python' },  -- can specify "all"
             highlight = {
               enable = true,
               disable = { 'python' },
             },
-          }
-        end,
-      }
-      use {
-        'nvim-treesitter/nvim-treesitter-textobjects',
-        requires = { 'nvim-treesitter/nvim-treesitter' },
-        config = function()
-          require('nvim-treesitter.configs').setup {
             textobjects = {
               select = {
                 enable = true,
@@ -224,6 +218,26 @@ return require('packer').startup {
                   ["if"] = "@function.inner",
                   ["ac"] = "@class.outer",
                   ["ic"] = "@class.inner",
+                },
+              },
+              move = {
+                enable = true,
+                set_jumps = true, -- whether to set jumps in the jumplist
+                goto_next_start = {
+                  ["]m"] = "@function.outer",
+                  ["]]"] = "@class.outer",
+                },
+                goto_next_end = {
+                  ["]M"] = "@function.outer",
+                  ["]["] = "@class.outer",
+                },
+                goto_previous_start = {
+                  ["[m"] = "@function.outer",
+                  ["[["] = "@class.outer",
+                },
+                goto_previous_end = {
+                  ["[M"] = "@function.outer",
+                  ["[]"] = "@class.outer",
                 },
               },
             },
