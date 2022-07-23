846042b7f6882a673325927e73a77a674f95d60b
kenny <kkihara1@gmail.com>
2022-07-23 22:25:49 +0000

    fix lua lsp path

--- vim/vim.symlink/lua/plugins.lua
+++ vim/vim.symlink/lua/plugins.lua
@@ -68,7 +68,7 @@ return require('packer').startup {
 
           -- LSP: sumneko_lua
           local sumneko_binary_path = vim.fn.resolve(vim.fn.exepath('lua-language-server'))
-          local sumneko_root_path = vim.fn.fnamemodify(sumneko_binary_path, ':h:h:h')
+          local sumneko_root_path = vim.fn.fnamemodify(sumneko_binary_path, ':h:h')
           local runtime_path = vim.split(package.path, ';')
           table.insert(runtime_path, 'lua/?.lua')
           table.insert(runtime_path, 'lua/?/init.lua')
