97e830ea49753a9a8e2aef94e8971e445d7e6034
kenny <kkihara1@gmail.com>
2022-11-10 00:12:32 -0800

    update

--- vim/vim.symlink/lua/plugins.lua
+++ vim/vim.symlink/lua/plugins.lua
@@ -157,7 +157,6 @@ return require('packer').startup {
           require('rust-tools').setup {
             tools = { -- rust-tools options
               autoSetHints = true,
-              hover_with_actions = true,
               inlay_hints = {
                   show_parameter_hints = false,
                   parameter_hints_prefix = "",
