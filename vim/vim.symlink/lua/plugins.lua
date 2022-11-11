deafc4d14a75f64fc4ce5233c218d4acc50dce77
kenny <kkihara1@gmail.com>
2022-11-11 10:40:18 -0800

    update

--- vim/vim.symlink/lua/plugins.lua
+++ vim/vim.symlink/lua/plugins.lua
@@ -415,14 +415,6 @@ return require('packer').startup {
         'junegunn/fzf.vim',
         requires = { 'junegunn/fzf', run = 'fzf#install()' }
       }
-      use {
-        'terryma/vim-smooth-scroll',
-        config = function()
-          vim.api.nvim_set_keymap('n', '<C-U>', ':call smooth_scroll#up(&scroll, 0, 3)<CR>', { nowait = true, silent = true })
-          vim.api.nvim_set_keymap('n',  '<C-D>', ':call smooth_scroll#down(&scroll, 0, 3)<CR>', { nowait = true, silent = true })
-        end,
-      }
-
       use { 'machakann/vim-swap' }
       use {
         'hoob3rt/lualine.nvim',
@@ -466,6 +458,33 @@ return require('packer').startup {
         'yamatsum/nvim-nonicons',
         requires = { 'kyazdani42/nvim-web-devicons' },
         config = function() require('nvim-nonicons').get('file') end,
+      }
+      use {
+        "lukas-reineke/indent-blankline.nvim",
+        config = function()
+          require("indent_blankline").setup {
+            space_char_blankline = " ",
+            show_current_context = true,
+            show_current_context_start = true,
+          }
+        end
+      }
+      use {
+        "karb94/neoscroll.nvim",
+        config = function()
+          require('neoscroll').setup({
+            -- All these keys will be mapped to their corresponding default scrolling animation
+            mappings = {'<C-u>', '<C-d>', '<C-b>', '<C-f>', '<C-y>', '<C-e>'},
+            hide_cursor = true,          -- Hide cursor while scrolling
+            stop_eof = true,             -- Stop at <EOF> when scrolling downwards
+            respect_scrolloff = false,   -- Stop scrolling when the cursor reaches the scrolloff margin of the file
+            cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
+            easing_function = nil,       -- Default easing function
+            pre_hook = nil,              -- Function to run before the scrolling animation starts
+            post_hook = nil,             -- Function to run after the scrolling animation ends
+            performance_mode = false,    -- Disable "Performance Mode" on all buffers.
+        })
+        end
       }
   end,
   config = {
