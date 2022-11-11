d3d5233eeded8d11dda46218283114157115634d
kenny <kkihara1@gmail.com>
2022-11-11 19:52:39 +0000

    update

--- install_scripts/install-nvim-links.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir -p "$HOME/.config"
-ln -s $HOME/.vimrc $HOME/.vim/init.vim
-ln -s $HOME/.vim $HOME/.config/nvim
--- install_scripts/install-ripgrep.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-curl -LO https://github.com/BurntSushi/ripgrep/releases/download/0.10.0/ripgrep_0.10.0_amd64.deb
-sudo dpkg -i ripgrep_0.10.0_amd64.deb
-rm ripgrep_0.10.0_amd64.deb
