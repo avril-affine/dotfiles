e30b3894b11c8d0b4dbb6502b34fa58f7971b243
kenny <kkihara1@gmail.com>
2023-05-06 22:37:17 -0700

    update

--- vim/vim.symlink/lua/plugins.lua
+++ vim/vim.symlink/lua/plugins.lua
@@ -102,53 +102,11 @@ return require('packer').startup {
             },
           }
 
-          -- LSP: lua
-          -- local sumneko_binary_path = vim.fn.resolve(vim.fn.exepath('lua-language-server'))
-          -- local sumneko_root_path = vim.fn.fnamemodify(sumneko_binary_path, ':h:h')
-          -- local runtime_path = vim.split(package.path, ';')
-          -- table.insert(runtime_path, 'lua/?.lua')
-          -- table.insert(runtime_path, 'lua/?/init.lua')
-          -- require('lspconfig').sumneko_lua.setup {
-          --   on_attach = on_attach,
-          --   cmd = {sumneko_binary_path, '-E', sumneko_root_path .. '/main.lua'},
-          --   settings = {
-          --       Lua = {
-          --         runtime = {
-          --             version = 'LuaJIT',
-          --             path = runtime_path,
-          --         },
-          --         diagnostics = {
-          --             -- Get the language server to recognize the `vim` global
-          --             globals = {'vim'},
-          --         },
-          --         workspace = {
-          --             -- Make the server aware of Neovim runtime files
-          --             library = vim.api.nvim_get_runtime_file('', true),
-          --         },
-          --         -- Do not send telemetry data containing a randomized but unique identifier
-          --         telemetry = {
-          --             enable = false,
-          --         },
-          --       },
-          --   },
-          -- }
-
-          -- LSP: dart
-          local dart_binary_path = vim.fn.resolve(vim.fn.exepath('dart'))
-          local dart_root_path = vim.fn.fnamemodify(dart_binary_path, ':h')
-          if string.find(dart_binary_path, 'flutter') then
-            dart_root_path = dart_root_path .. '/cache/dart-sdk/bin'
-          end
-          require('lspconfig').dartls.setup {
-            on_attach = on_attach,
-            cmd = { dart_binary_path, dart_root_path .. '/snapshots/analysis_server.dart.snapshot', '--lsp' },
-          }
-
           -- LSP: rust
-          -- local rust_analyzer_binary_path = vim.fn.resolve(vim.fn.exepath('rust-analyzer'))
-          -- require('lspconfig').rust_analyzer.setup {
-          --   on_attach = on_attach,
-          -- }
+          local rust_analyzer_binary_path = vim.fn.resolve(vim.fn.exepath('rust-analyzer'))
+          require('lspconfig').rust_analyzer.setup {
+            on_attach = on_attach,
+          }
 
           -- LSP: typescript
           require('lspconfig').tsserver.setup {
