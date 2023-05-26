630730340fddbfb489c4f4cc17b82766eba796d8
kenny <kkihara1@gmail.com>
2023-05-26 14:31:55 -0700

    update

--- vim/vim.symlink/lua/plugins/lsp.lua
+++ vim/vim.symlink/lua/plugins/lsp.lua
@@ -153,7 +153,7 @@ return {
             require("lazyvim.plugins.lsp.format").autoformat = opts.autoformat
             -- setup formatting and keymaps
             Util.on_attach(function(client, buffer)
-                require("lazyvim.plugins.lsp.format").on_attach(client, buffer)
+                -- require("lazyvim.plugins.lsp.format").on_attach(client, buffer)
                 require("lazyvim.plugins.lsp.keymaps").on_attach(client, buffer)
             end)
 
