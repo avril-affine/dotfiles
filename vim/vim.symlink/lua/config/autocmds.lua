eb1c071dbc26fd2f15147205324f4ba5100c93b8
kenny <kkihara1@gmail.com>
2023-05-25 19:08:29 -0700

    update

--- vim/vim.symlink/lua/config/autocmds.lua
+++ vim/vim.symlink/lua/config/autocmds.lua
@@ -4,15 +4,22 @@ local augroup = vim.api.nvim_create_augroup
 -- turn off lsp_lines by default
 -- autocmd("VimEnter", { callback = function() require("lsp_lines").toggle() end })
 
--- highlight cursor line
-vim.api.nvim_set_hl(0, "CursorLine", { cterm = { underline = true } })
-vim.cmd.set("cursorline")
-augroup("CursorLine", { clear = true })
+
+-- vim.api.nvim_set_hl(0, "CursorLine", { cterm = { underline = true } })
+-- augroup("CursorLine", { clear = true })
+-- autocmd({ "VimEnter", "WinEnter", "BufWinEnter" }, {
+--     group = "CursorLine",
+--     command = "setlocal cursorline",
+-- })
+-- autocmd("WinLeave", {
+--     group = "CursorLine",
+--     command = "setlocal nocursorline",
+-- })
+
+-- relative line number *only* in current window
 autocmd({ "VimEnter", "WinEnter", "BufWinEnter" }, {
-    group = "CursorLine",
-    command = "setlocal cursorline",
+    command = "setlocal relativenumber",
 })
 autocmd("WinLeave", {
-    group = "CursorLine",
-    command = "setlocal nocursorline",
+    command = "setlocal norelativenumber",
 })
--- vim/vim.symlink/lua/plugins/colorscheme.lua
+++ vim/vim.symlink/lua/plugins/colorscheme.lua
@@ -1,19 +1,30 @@
 return {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-        require("tokyonight").setup({
-        style = "night",
-        dim_inactive = true,
-        on_highlights = function(hl, c)
-          hl.Normal = { fg = c.white, bg = c.bg }
-          hl.Comment = { fg = '#ffdfaf' }
-          hl.LineNr = { fg = c.white, bg = '#4c4e52' }
-          hl.CursorLineNr = { fg = c.cyan }
-          hl.SignColumn = { bg = '#4c4e52' }
-        end,
-        })
-        vim.cmd.colorscheme("tokyonight")
-    end,
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            require("tokyonight").setup({
+                 style = "night",
+                 dim_inactive = true,
+                 on_highlights = function(hl, c)
+                     hl.Normal = { fg = "#ffffff" , bg = c.bg }
+                     hl.Comment = { fg = "#ffdfaf" }
+                     hl.LineNr = { fg = "#ffffff", bg = "#4c4e52" }
+                     hl.CursorLineNr = { fg = c.cyan, bg = "#4c4e52", bold = true }
+                     hl.SignColumn = { bg = "#4c4e52" }
+                 end,
+            })
+            vim.cmd.colorscheme("tokyonight")
+
+            -- highlight cursorline
+            vim.cmd.set("cursorline")
+            vim.api.nvim_create_autocmd({ "VimEnter", "WinEnter", "BufWinEnter" }, {
+                command = "setlocal cursorline",
+            })
+            vim.api.nvim_create_autocmd("WinLeave", {
+                command = "setlocal nocursorline",
+            })
+        end
+    }
 }
