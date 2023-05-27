7b2c3d5c184ff701128a0f3ee97e94bc7ad092ed
kenny <kkihara1@gmail.com>
2023-05-26 20:39:36 -0700

    update

--- vim/vim.symlink/lua/plugins/navigation.lua
+++ vim/vim.symlink/lua/plugins/navigation.lua
@@ -42,99 +42,105 @@ return {
 	},
 	{
 		"nvim-treesitter/nvim-treesitter",
-		dependencies = { "nvim-treesitter/nvim-treesitter-textobjects", "nvim-treesitter/playground" },
+		dependencies = {
+            "nvim-treesitter/nvim-treesitter-textobjects",
+            "nvim-treesitter/nvim-treesitter-context",
+            "nvim-treesitter/playground",
+        },
 		build = ":TSUpdate",
-		init = function()
-			require("nvim-treesitter.configs").setup({
-				ensure_installed = {
-                    "bash",
-                    "html",
-                    "json",
-                    "luadoc",
-                    "markdown",
-                    "python",
-                    "regex",
-                    "tsx",
-                    "vim",
-                    "yaml",
-                    "c",
-                    "javascript",
-                    "lua",
-                    "luap",
-                    "markdown_inline",
-                    "query",
-                    "rust",
-                    "typescript",
-                    "vimdoc",
+		opts = {
+            ensure_installed = {
+                "bash",
+                "html",
+                "json",
+                "luadoc",
+                "markdown",
+                "python",
+                "regex",
+                "tsx",
+                "vim",
+                "yaml",
+                "c",
+                "javascript",
+                "lua",
+                "luap",
+                "markdown_inline",
+                "query",
+                "rust",
+                "typescript",
+                "vimdoc",
+            },
+            -- highlights
+            highlight = {
+                enable = true,
+                disable = { "python" },
+            },
+            query_linter = {
+                enable = true,
+                use_virtual_text = true,
+                lint_events = { "BufWrite", "CursorHold" },
+            },
+            -- text objects
+            textobjects = {
+                select = {
+                    enable = true,
+
+                    -- Automatically jump forward to textobj, similar to targets.vim
+                    lookahead = true,
+
+                    keymaps = {
+                        -- You can use the capture groups defined in textobjects.scm
+                        ["af"] = "@function.outer",
+                        ["if"] = "@function.inner",
+                        ["ac"] = "@class.outer",
+                        ["ic"] = "@class.inner",
+                    },
                 },
-				-- highlights
-				highlight = {
-					enable = true,
-					disable = { "python" },
-				},
-				-- text objects
-				textobjects = {
-					select = {
-						enable = true,
-
-						-- Automatically jump forward to textobj, similar to targets.vim
-						lookahead = true,
-
-						keymaps = {
-							-- You can use the capture groups defined in textobjects.scm
-							["af"] = "@function.outer",
-							["if"] = "@function.inner",
-							["ac"] = "@class.outer",
-							["ic"] = "@class.inner",
-						},
-					},
-					move = {
-						enable = true,
-						set_jumps = true, -- whether to set jumps in the jumplist
-						goto_next_start = {
-							["]m"] = "@function.outer",
-							["]]"] = "@class.outer",
-						},
-						goto_next_end = {
-							["]M"] = "@function.outer",
-							["]["] = "@class.outer",
-						},
-						goto_previous_start = {
-							["[m"] = "@function.outer",
-							["[["] = "@class.outer",
-						},
-						goto_previous_end = {
-							["[M"] = "@function.outer",
-							["[]"] = "@class.outer",
-						},
-					},
-				},
-				query_linter = {
-					enable = true,
-					use_virtual_text = true,
-					lint_events = { "BufWrite", "CursorHold" },
-				},
-				-- playground
-				playground = {
-					enable = true,
-					disable = {},
-					updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-					persist_queries = false, -- Whether the query persists across vim sessions
-					keybindings = {
-						toggle_query_editor = "o",
-						toggle_hl_groups = "i",
-						toggle_injected_languages = "t",
-						toggle_anonymous_nodes = "a",
-						toggle_language_display = "I",
-						focus_language = "f",
-						unfocus_language = "F",
-						update = "R",
-						goto_node = "<cr>",
-						show_help = "?",
-					},
-				},
-			})
-		end,
+                move = {
+                    enable = true,
+                    set_jumps = true, -- whether to set jumps in the jumplist
+                    goto_next_start = {
+                        ["]m"] = "@function.outer",
+                        ["]]"] = "@class.outer",
+                    },
+                    goto_next_end = {
+                        ["]M"] = "@function.outer",
+                        ["]["] = "@class.outer",
+                    },
+                    goto_previous_start = {
+                        ["[m"] = "@function.outer",
+                        ["[["] = "@class.outer",
+                    },
+                    goto_previous_end = {
+                        ["[M"] = "@function.outer",
+                        ["[]"] = "@class.outer",
+                    },
+                },
+            },
+            -- context
+            context = {
+                multiline_threshold = 80,
+            },
+            -- playground
+            playground = {
+                enable = true,
+                disable = {},
+                updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+                persist_queries = false, -- Whether the query persists across vim sessions
+                keybindings = {
+                    toggle_query_editor = "o",
+                    toggle_hl_groups = "i",
+                    toggle_injected_languages = "t",
+                    toggle_anonymous_nodes = "a",
+                    toggle_language_display = "I",
+                    focus_language = "f",
+                    unfocus_language = "F",
+                    update = "R",
+                    goto_node = "<cr>",
+                    show_help = "?",
+                },
+            },
+        }
 	},
 	{
 		"machakann/vim-swap",
@@ -169,10 +175,8 @@ return {
     },
     {
         "echasnovski/mini.bufremove",
-        -- stylua: ignore
         keys = {
             { "<leader>bd", function() require("mini.bufremove").delete(0, false) end, desc = "Delete Buffer" },
-            { "<leader>bD", function() require("mini.bufremove").delete(0, true) end, desc = "Delete Buffer (Force)" },
         },
     },
     {
