9b3888ce2ce8964c3fb7a5595cfd2b2dfdda12f8
kenny <kkihara1@gmail.com>
2023-08-24 00:24:27 -0700

    update

--- vim/vim.symlink/lua/plugins/lsp.lua
+++ vim/vim.symlink/lua/plugins/lsp.lua
@@ -38,7 +38,6 @@ return {
         event = { "BufReadPre", "BufNewFile" },
         dependencies = {
             "LazyVim/LazyVim",
-            { "folke/neoconf.nvim", cmd = "Neoconf", config = true },
             { "folke/neodev.nvim", opts = {} },
             "mason.nvim",
             "williamboman/mason-lspconfig.nvim",
@@ -311,6 +310,7 @@ return {
                 }),
                 sources = cmp.config.sources({
                     { name = "nvim_lsp" },
+                    { name = "nvim_lsp_signature_help" },
                     { name = "luasnip" },
                     { name = "buffer" },
                     { name = "path" },
@@ -318,4 +318,67 @@ return {
             }
         end,
     },
+    -- lazy.nvim
+    {
+        "folke/noice.nvim",
+        dependencies = {
+            "MunifTanjim/nui.nvim",
+        },
+        event = "VeryLazy",
+        opts = {
+            messages = { enabled = false },
+            notify = { enabled = false },
+            presets = {
+                bottom_search = true,
+            },
+            lsp = {
+                progress = {
+                    enabled = true,
+                    -- Lsp Progress is formatted using the builtins for lsp_progress. See config.format.builtin
+                    -- See the section on formatting for more details on how to customize.
+                    --- @type NoiceFormat|string
+                    format = "lsp_progress",
+                    --- @type NoiceFormat|string
+                    format_done = "lsp_progress_done",
+                    throttle = 1000 / 30, -- frequency to update lsp progress message
+                    view = "mini",
+                },
+                override = {
+                    -- override cmp documentation with Noice (needs the other options to work)
+                    ["cmp.entry.get_documentation"] = true,
+                },
+                hover = {
+                    enabled = true,
+                    silent = false, -- set to true to not show a message if hover is not available
+                    view = nil, -- when nil, use defaults from documentation
+                    ---@type NoiceViewOptions
+                    opts = {}, -- merged with defaults from documentation
+                },
+                signature = {
+                    enabled = true,
+                    auto_open = {
+                        enabled = true,
+                        trigger = true, -- Automatically show signature help when typing a trigger character from the LSP
+                        luasnip = true, -- Will open signature help when jumping to Luasnip insert nodes
+                        throttle = 50, -- Debounce lsp signature help request by 50ms
+                    },
+                    view = nil, -- when nil, use defaults from documentation
+                    ---@type NoiceViewOptions
+                    opts = {}, -- merged with defaults from documentation
+                },
+                -- defaults for hover and signature help
+                documentation = {
+                    view = "hover",
+                    ---@type NoiceViewOptions
+                    opts = {
+                        lang = "markdown",
+                        replace = true,
+                        render = "plain",
+                        format = { "{message}" },
+                        win_options = { concealcursor = "n", conceallevel = 3 },
+                    },
+                },
+            },
+        },
+    }
 }
