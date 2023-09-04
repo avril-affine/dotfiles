4e503dfecbccad28ff86b231953e0b76840f7be0
kenny <kkihara1@gmail.com>
2023-09-03 19:57:40 -0700

    update

--- vim/vim.symlink/lua/plugins/navigation.lua
+++ vim/vim.symlink/lua/plugins/navigation.lua
@@ -156,36 +156,17 @@ return {
 	},
     {
         "echasnovski/mini.surround",
-        keys = function(_, keys)
-            -- Populate the keys based on the user's options
-            local plugin = require("lazy.core.config").spec.plugins["mini.surround"]
-            local opts = require("lazy.core.plugin").values(plugin, "opts", false)
-            local mappings = {
-                { opts.mappings.add, desc = "Add surrounding", mode = { "n", "v" } },
-                -- { opts.mappings.delete, desc = "Delete surrounding" },
-                -- { opts.mappings.find, desc = "Find right surrounding" },
-                -- { opts.mappings.find_left, desc = "Find left surrounding" },
-                -- { opts.mappings.replace, desc = "Replace surrounding" },
-            }
-            mappings = vim.tbl_filter(function(m)
-                return m[1] and #m[1] > 0
-            end, mappings)
-            return vim.list_extend(mappings, keys)
-        end,
+        keys = {
+            { "<leader>sa", desc = "Add surrounding", mode = { "n", "v" } },
+            { "<leader>sd", desc = "Delete surrounding", mode = { "n", "v" } },
+            { "<leader>ss", desc = "Replace surrounding", mode = { "n", "v" } },
+        },
         opts = {
             mappings = {
-                add = "Sa", -- Add surrounding in Normal and Visual modes
-                -- delete = "Sd", -- Delete surrounding
-                -- find = "Sf", -- Find surrounding (to the right)
-                -- find_left = "SF", -- Find surrounding (to the left)
-                -- replace = "Sr", -- Replace surrounding
+                add = "<leader>sa",
+                delete = "<leader>sd",
+                replace = "<leader>ss",
             },
-        },
-    },
-    {
-        "echasnovski/mini.bufremove",
-        keys = {
-            { "<leader>bd", function() require("mini.bufremove").delete(0, false) end, desc = "Delete Buffer" },
         },
     },
     {
@@ -195,9 +176,8 @@ return {
             "nvim-tree/nvim-web-devicons",
         },
         keys = {
-            { "<leader>a", "<cmd>AerialToggle!<CR>", desc = "AerialToggle" },
-            { "{", "<cmd>AerialPrev<CR>", desc = "AerialPrev" },
-            { "}", "<cmd>AerialNext<CR>", desc = "AerialNext" },
+            { "<leader>a", "<cmd>AerialToggle!<CR>", desc = "Navigate blocks side pane" },
+            { "<leader>A", "<cmd>AerialNavToggle<CR>", desc = "Navigate blocks" },
         },
         opts = {
             backends = { "treesitter" },
@@ -206,5 +186,8 @@ return {
     },
     {
         "tpope/vim-fugitive",
+        keys = {
+            { "<leader>g", "<cmd>Git<CR>", desc = "Git status", mode = "n" },
+        },
     },
 }
