dabdd714aa894562083463b9864a15ac64cd6e0a
kenny <kkihara1@gmail.com>
2023-12-28 16:44:21 -0800

    fix

--- vim/vim.symlink/lua/util.lua
+++ vim/vim.symlink/lua/util.lua
@@ -1,10 +1,27 @@
 local M = {}
 
+local function _print_table(tbl, indent)
+    if not indent then indent = 0 end
+    for k, v in pairs(tbl) do
+        formatting = string.rep("  ", indent) .. k .. ": "
+        if type(v) == "table" then
+            print(formatting)
+            tprint(v, indent+1)
+        elseif type(v) == 'boolean' then
+            print(formatting .. tostring(v))
+        else
+            print(formatting .. v)
+        end
+    end
+end
+
 M.project_dir = function()
+    local current_dir = vim.fs.dirname(string.sub(debug.getinfo(1).source, 2))
+    local realpath = io.popen("realpath " .. current_dir):read("a")
     local git_path = vim.fs.find(".git", {
         upward = true,
         stop = vim.loop.os_homedir(),
-        path = vim.fs.dirname(vim.api.nvim_buf_get_name(0)),
+        path = realpath,
         type = "directory",
     })[1]
     return vim.fs.dirname(git_path)
