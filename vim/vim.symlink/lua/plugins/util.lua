adfc642aa49e5cd5ca7eb782feeba17f910d3785
kenny <kkihara1@gmail.com>
2024-01-23 13:18:33 -0800

    ccls

--- vim/vim.symlink/lua/plugins/util.lua
+++ vim/vim.symlink/lua/plugins/util.lua
@@ -82,4 +82,23 @@ return {
         },
         opts = {},
     },
+    {
+        "numToStr/Comment.nvim",
+        lazy = false,
+        opts = {
+            mappings = { basic = false, extra = false },
+        },
+        keys = {
+            {
+                "/",
+                function()
+                    local esc = vim.api.nvim_replace_termcodes("<ESC>", true, false, true)
+                    vim.api.nvim_feedkeys(esc, "nx", false)
+                    require("Comment.api").toggle.linewise(vim.fn.visualmode())
+                end,
+                mode = "v",
+                { desc = "Comment toggle linewise (visual)" },
+            },
+        }
+    },
 }
