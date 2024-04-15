5c3262c82fb3406e0782d34ea37d2419cf6b2817
kenny <kkihara1@gmail.com>
2024-04-14 18:52:30 -0700

    update

--- vim/vim.symlink/lua/config/autocmds.lua
+++ vim/vim.symlink/lua/config/autocmds.lua
@@ -1,20 +1,4 @@
 local autocmd = vim.api.nvim_create_autocmd
-local augroup = vim.api.nvim_create_augroup
-
--- turn off lsp_lines by default
--- autocmd("VimEnter", { callback = function() require("lsp_lines").toggle() end })
-
-
--- vim.api.nvim_set_hl(0, "CursorLine", { cterm = { underline = true } })
--- augroup("CursorLine", { clear = true })
--- autocmd({ "VimEnter", "WinEnter", "BufWinEnter" }, {
---     group = "CursorLine",
---     command = "setlocal cursorline",
--- })
--- autocmd("WinLeave", {
---     group = "CursorLine",
---     command = "setlocal nocursorline",
--- })
 
 -- relative line number *only* in current window
 autocmd({ "VimEnter", "WinEnter", "BufWinEnter" }, {
