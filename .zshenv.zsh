69d5b0230b1e62e4c450a13113253b00590e5d8e
panda <kkihara1@gmail.com>
2024-10-06 01:24:56 -0700

    move to stow format

--- .zshenv.zsh
+++ .zshenv.zsh
@@ -1,4 +1,4 @@
 export ZSH="$HOME/.oh-my-zsh"
-export RIPGREP_CONFIG_PATH="$HOME/.ripgreprc"
+export RIPGREP_CONFIG_PATH="$HOME/.config/ripgrep/.ripgreprc"
 export EDITOR="$(which nvim)"
 export fpath=($HOME/.zfunctions $fpath)
