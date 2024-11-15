62615a7a25dc314dbd34162263edd7463928685c
kenny <kkihara1@gmail.com>
2024-11-15 12:14:35 -0800

    update

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -8,6 +8,7 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.cursorline = true
 vim.opt.ruler = true -- row/col number
 vim.opt.timeout = false
 vim.opt.ttimeout = true
@@ -15,19 +16,25 @@ vim.opt.timeoutlen = 10
 vim.opt.clipboard = "unnamedplus" -- global clipboard
 vim.opt.backspace = "indent,eol,start" -- fix backspace
 
--- relative line number *only* in current window
+
 vim.api.nvim_create_autocmd({ "VimEnter", "WinEnter", "BufWinEnter" }, {
-    command = "setlocal relativenumber",
+    callback = function()
+        vim.opt_local.relativenumber = true  -- relative line number *only* in current window
+        vim.opt_local.cursorline = true  -- highlight cursorline
+    end,
 })
 vim.api.nvim_create_autocmd("WinLeave", {
-    command = "setlocal norelativenumber",
+    callback = function()
+        vim.opt_local.relativenumber = false
+        vim.opt_local.cursorline = false
+    end,
 })
 
--- highlight cursorline
-vim.cmd.set("cursorline")
-vim.api.nvim_create_autocmd({ "VimEnter", "WinEnter", "BufWinEnter" }, {
-    command = "setlocal cursorline",
-})
-vim.api.nvim_create_autocmd("WinLeave", {
-    command = "setlocal nocursorline",
+vim.api.nvim_create_autocmd({ "FileType" }, {
+    pattern = "json",
+    callback = function()
+        vim.opt_local.foldmethod = "expr"
+        vim.opt_local.foldexpr = "nvim_treesitter#foldexpr()"
+        vim.opt_local.foldlevel = 2
+    end,
 })
