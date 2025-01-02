a0fce6117e8d3b3de552b8c1c98fe5cd8533e8fb
kenny <kkihara1@gmail.com>
2025-01-01 17:03:48 -0800

    update

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,7 +1,17 @@
 return {
 	{
 		"nvim-telescope/telescope.nvim",
-		dependencies = { "nvim-lua/plenary.nvim" },
+		dependencies = {
+            "nvim-lua/plenary.nvim",
+            {
+                "nvim-telescope/telescope-fzf-native.nvim",
+                dependencies = { "nvim-telescope/telescope.nvim" },
+                build = "make",
+                config = function()
+                    require("telescope").load_extension("fzf")
+                end,
+            },
+        },
         opts = function(_, opts)
             local actions = require("telescope.actions")
             return {
@@ -13,6 +23,20 @@ return {
                         case_mode = "respect_case",
                     },
                 },
+                pickers = {
+                    find_files = {
+                        theme = "ivy",
+                    },
+                    help_tags = {
+                        theme = "ivy",
+                    },
+                    registers = {
+                        theme = "ivy",
+                    },
+                    live_grep = {
+                        theme = "ivy",
+                    },
+                },
                 defaults = {
                     mappings = {
                         n  = {
@@ -20,23 +44,28 @@ return {
                             ["<leader>qF"] = actions.smart_send_to_qflist + actions.open_qflist,
                         },
                     },
+                    vimgrep_arguments = {
+                        "rg",
+                        "--color=never",
+                        "--no-heading",
+                        "--with-filename",
+                        "--line-number",
+                        "--column",
+                    },
                 },
             }
         end,
         keys = {
             { "<C-p>", function() require("telescope.builtin").find_files() end, desc = "Open file finder." },
-            { ",p", function() require("telescope.builtin").registers() end, desc = "Open registers and paste." },
-        },
-	},
-	{
-		"nvim-telescope/telescope-fzf-native.nvim",
-		dependencies = { "nvim-telescope/telescope.nvim" },
-		build = "make",
-		config = function()
-			require("telescope").load_extension("fzf")
-		end,
-        keys = {
-            { "<leader>f", function() require("telescope.builtin").live_grep() end, desc = "Live grep." },
+            { "<leader>p", function() require("telescope.builtin").registers() end, desc = "Open registers and paste." },
+            {
+                "<leader>f",
+                function()
+                    require("telescope.builtin").live_grep({glob_pattern = {"!*.json", "!*.jsonl", "!*.dot", "!*log*"}})
+                end,
+                desc = "Live grep, ignore log files."
+            },
+            { "<leader>h", function() require("telescope.builtin").help_tags() end, desc = "Help tags." },
         },
 	},
 }
