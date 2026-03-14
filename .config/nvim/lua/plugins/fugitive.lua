f8beae324ed15deece2591313e38cff780f4cf5f
kenny <kkihara1@gmail.com>
2026-03-14 15:15:43 -0700

    update

--- .config/nvim/lua/plugins/fugitive.lua
+++ .config/nvim/lua/plugins/fugitive.lua
@@ -3,7 +3,8 @@ return {
     dependencies = { "tpope/vim-rhubarb" },
     lazy = false,
     keys = {
-        { "<leader>g", "<cmd>Git<CR>", desc = "Git status", mode = "n" },
+        { "<leader>g", "<cmd>Git<CR>", desc = "Git", mode = "n" },
         { "<leader>gl", "<cmd>G log --decorate<CR>", desc = "Git status", mode = "n" },
+        { "<leader>gd", "<cmd>Gvdiffsplit<CR>", desc = "Git diff", mode = "n" },
     },
 }
